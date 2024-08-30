293a7b4de425d493b5ab0b38036e8584f2d1ba6a
mahsa <mkmi7171@gmail.com>
2024-08-30 12:31:29 -0400

    middleware error handler

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from '../utils/error.js';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res,next) => {
     // console.log(req.body)
     const { username, email, password } = req.body;
 
@@ -13,6 +14,6 @@ export const signup = async (req, res) => {
     res.status(201).json("user created successfully!")
        
     } catch (error){
-        res.status(500).json(error.message)
+        next(errorHandler(550, "error from function"))
    }
 }
