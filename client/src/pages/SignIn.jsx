6c7984f578f36cf7b140c7b3ae409393991045c1
mahsa <mkmi7171@gmail.com>
2024-09-03 19:39:29 -0400

    add redux toolkit to the client side

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { signInStart, signInFailure, signInSuccess } from '../redux/user/userSlice'
+
 
 export default function SignIn() {
   const [formData, setFormData] = useState({})
   const [error, setError] = useState(null)
   const [loading, setLoading] = useState(false)
   const navigate = useNavigate()
+  const dispatch = useDispatch()
   
   const handleChange = (e) => {
     setFormData({
@@ -18,7 +22,7 @@ export default function SignIn() {
   const handleSubmit =async (e) => {
     e.preventDefault()
     try {
-     setLoading(true)
+     dispatch(signInStart)
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -28,16 +32,13 @@ export default function SignIn() {
       })
       const data = await res.json()
       if (data.success === false) {
-        setLoading(false)
-        setError(data.message)
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false)
-      setError(null)
+      dispatch(signInSuccess(data))
       navigate('/')
    } catch (error) {
-      setLoading(false)
-      setError(error.message)
+        dispatch(signInFailure(error.message));
    }
   }
   return (
